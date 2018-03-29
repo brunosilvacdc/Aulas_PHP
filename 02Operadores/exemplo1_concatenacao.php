7b0ade77e4fa5487928830c3af289f0344392920
Bruno Silva <brunosilvacdc@gmail.com>
2018-03-29 10:57:40 -0300

    aulas Udemy

---
+++ 02Operadores/exemplo1_concatenacao.php
@@ -0,0 +1,16 @@
+<?php
+$nome = "Hcode";
+
+echo $nome . " mais alguma coisa <br><br><hr><br><br>";
+
+$nome .= "Treinamentos";
+
+echo $nome;
+
+function teste(){
+
+	return 2+3;
+}
+
+
+echo teste();
